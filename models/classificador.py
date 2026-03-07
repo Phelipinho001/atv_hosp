84305c21665adf3245094880eac05ed929742bb1
Gabriel Felipe Freire Silva <gabeielfelipe892@gmail.com>
2026-03-06 21:45:36 -0300

    update

--- models/classificador.py
+++ models/classificador.py
@@ -1,21 +1,13 @@
 from models.paciente import Pacient
 
 class Classificador():
-
     @staticmethod
-
-    def classificar(Paciente : Pacient):
-        if Paciente.pcd == True or Paciente.idade >=60:
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+    def classificar(paciente: Pacient):
+        
+        if paciente.pcd == True or paciente.idade >= 60:
             return "PRIORITÁRIO (Lei 10.048)"
-=======
-=======
->>>>>>> Stashed changes
-            return "Prioridade"
->>>>>>> Stashed changes
-        
         else:
-            return "Normal"
-        
+            return "Geral"
+
+
 classificar = Classificador.classificar
